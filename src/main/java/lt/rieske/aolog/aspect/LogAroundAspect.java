98f06191b8f834fb6d95ec9e5b808dd2f2e89269
rieske <vaidotas.valuckas@gmail.com>
2014-02-17 20:13:57 +0200

    corrected formatting

--- src/main/java/lt/rieske/aolog/aspect/LogAroundAspect.java
+++ src/main/java/lt/rieske/aolog/aspect/LogAroundAspect.java
@@ -17,50 +17,49 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LogAroundAspect {
 
-	@Autowired
-	private AspectLoggerFactory loggerFactory;
+    @Autowired
+    private AspectLoggerFactory loggerFactory;
 
-	private static final String VOID_RETURN = "void ";
+    private static final String VOID_RETURN = "void ";
 
-	@Around("execution(public * *(..)) && @within(configuration)")
-	public void logAroundAnnotatedClassPublicMethods(ProceedingJoinPoint joinPoint, LogAround configuration) throws Throwable {
-		logAround(joinPoint, configuration);
-	}
+    @Around("execution(public * *(..)) && @within(configuration)")
+    public void logAroundAnnotatedClassPublicMethods(ProceedingJoinPoint joinPoint, LogAround configuration) throws Throwable {
+        logAround(joinPoint, configuration);
+    }
 
-	@Around("execution(* *(..)) && @annotation(configuration)")
-	public void logAroundAnnotatedMethods(ProceedingJoinPoint joinPoint, LogAround configuration) throws Throwable {
-		logAround(joinPoint, configuration);
-	}
+    @Around("execution(* *(..)) && @annotation(configuration)")
+    public void logAroundAnnotatedMethods(ProceedingJoinPoint joinPoint, LogAround configuration) throws Throwable {
+        logAround(joinPoint, configuration);
+    }
 
-	private void logAround(ProceedingJoinPoint joinPoint, LogAround configuration) throws Throwable {
-		Signature methodSignature = joinPoint.getSignature();
-		AroundMethodLogger logger = loggerFactory.getAroundMethodLogger(methodSignature, joinPoint.getTarget(),
-				configuration);
+    private void logAround(ProceedingJoinPoint joinPoint, LogAround configuration) throws Throwable {
+        Signature methodSignature = joinPoint.getSignature();
+        AroundMethodLogger logger = loggerFactory.getAroundMethodLogger(methodSignature, joinPoint.getTarget(), configuration);
 
-		logBefore(logger, joinPoint);
-		try {
-			Object returnValue = joinPoint.proceed();
-			logAfter(logger, methodSignature, returnValue);
-		} catch (Exception e) {
-			logger.logException(e);
-			throw e;
-		}
-	}
+        logBefore(logger, joinPoint);
+        try {
+            Object returnValue = joinPoint.proceed();
+            logAfter(logger, methodSignature, returnValue);
+        } catch (Exception e) {
+            logger.logException(e);
+            throw e;
+        }
+    }
 
-	private void logBefore(AroundMethodLogger logger, ProceedingJoinPoint joinPoint) {
-		Object[] arguments = joinPoint.getArgs();
-		if (arguments.length == 0) {
-			logger.logBefore();
-		} else {
-			logger.logBefore(Arrays.toString(arguments));
-		}
-	}
+    private void logBefore(AroundMethodLogger logger, ProceedingJoinPoint joinPoint) {
+        Object[] arguments = joinPoint.getArgs();
+        if (arguments.length == 0) {
+            logger.logBefore();
+        } else {
+            logger.logBefore(Arrays.toString(arguments));
+        }
+    }
 
-	private void logAfter(AroundMethodLogger logger, Signature methodSignature, Object returnValue) {
-		if (methodSignature.toString().startsWith(VOID_RETURN)) {
-			logger.logAfter();
-		} else {
-			logger.logAfter(String.valueOf(returnValue));
-		}
-	}
+    private void logAfter(AroundMethodLogger logger, Signature methodSignature, Object returnValue) {
+        if (methodSignature.toString().startsWith(VOID_RETURN)) {
+            logger.logAfter();
+        } else {
+            logger.logAfter(String.valueOf(returnValue));
+        }
+    }
 }
--- src/main/java/lt/rieske/aolog/logger/AspectLoggerFactory.java
+++ src/main/java/lt/rieske/aolog/logger/AspectLoggerFactory.java
@@ -13,34 +13,33 @@ import org.springframework.util.StopWatch;
 
 public abstract class AspectLoggerFactory {
 
-	@Autowired
-	private LoggerFactoryWrapper loggerFactory;
+    @Autowired
+    private LoggerFactoryWrapper loggerFactory;
 
-	private static enum LoggerType {
-		TRACE, PERF
-	}
+    private static enum LoggerType {
+        TRACE, PERF
+    }
 
-	private final Map<String, LoggerType> loggerMappings = new HashMap<>();
+    private final Map<String, LoggerType> loggerMappings = new HashMap<>();
 
-	public AspectLoggerFactory() {
-		loggerMappings.put("trace", LoggerType.TRACE);
-		loggerMappings.put("perf", LoggerType.PERF);
-	}
+    public AspectLoggerFactory() {
+        loggerMappings.put("trace", LoggerType.TRACE);
+        loggerMappings.put("perf", LoggerType.PERF);
+    }
 
-	public AroundMethodLogger getAroundMethodLogger(Signature methodSignature, Object target, LogAround configuration) {
-		LoggerWrapper loggerWrapper = LoggerWrapper.getLoggerWrapper(configuration.logLevel());
-		Logger logger = loggerFactory.getLogger(target.toString());
-		String loggerTypeString = configuration.value().toLowerCase();
-		LoggerType loggerType = loggerMappings.get(loggerTypeString);
-		if (loggerType != null) {
-			return getAroundMethodLogger(loggerType, loggerWrapper, logger, methodSignature);
-		} else {
-			return getCustomAroundMethodLogger(loggerTypeString, loggerWrapper, logger, methodSignature);
-		}
-	}
+    public AroundMethodLogger getAroundMethodLogger(Signature methodSignature, Object target, LogAround configuration) {
+        LoggerWrapper loggerWrapper = LoggerWrapper.getLoggerWrapper(configuration.logLevel());
+        Logger logger = loggerFactory.getLogger(target.toString());
+        String loggerTypeString = configuration.value().toLowerCase();
+        LoggerType loggerType = loggerMappings.get(loggerTypeString);
+        if (loggerType != null) {
+            return getAroundMethodLogger(loggerType, loggerWrapper, logger, methodSignature);
+        } else {
+            return getCustomAroundMethodLogger(loggerTypeString, loggerWrapper, logger, methodSignature);
+        }
+    }
 
-    private AroundMethodLogger getAroundMethodLogger(LoggerType loggerType, LoggerWrapper loggerWrapper, Logger logger,
-            Signature methodSignature) {
+    private AroundMethodLogger getAroundMethodLogger(LoggerType loggerType, LoggerWrapper loggerWrapper, Logger logger, Signature methodSignature) {
         switch (loggerType) {
         case PERF:
             return new AroundMethodPerfLogger(loggerWrapper, logger, methodSignature, new StopWatch());
@@ -50,6 +49,5 @@ public abstract class AspectLoggerFactory {
         }
     }
 
-    protected abstract AroundMethodLogger getCustomAroundMethodLogger(String loggerType, LoggerWrapper loggerWrapper,
-			Logger logger, Signature methodSignature);
+    protected abstract AroundMethodLogger getCustomAroundMethodLogger(String loggerType, LoggerWrapper loggerWrapper, Logger logger, Signature methodSignature);
 }
